68e0a6eeb0de68ceb22443155fe4c9da7fb75280
valerio <romavalerioio@gmail.com>
2021-11-26 07:38:28 +0000

    changed prisoners type from tuple to array

--- authored/100 prisoners and a light bulb/example-test.py
+++ authored/100 prisoners and a light bulb/example-test.py
@@ -5,13 +5,13 @@ from solution import living_room
 def _():
     @test.it('Fixed test')
     def _():
-        prisoners = [() for _ in range(100)]
+        prisoners = [[] for _ in range(100)]
         lightbulb = False
         assertion = False
         for i in range(29200):
             light_before = lightbulb
             lightbulb, assertion = living_room(i % 100, lightbulb, prisoners[i % 100])
-            prisoners[i % 100] = (*prisoners[i % 100], light_before)
+            prisoners[i % 100].append(light_before)
             if assertion:
                 test.expect(i >= 99, 'One of the prisoners made a false assertion')
                 break
--- authored/100 prisoners and a light bulb/test.py
+++ authored/100 prisoners and a light bulb/test.py
@@ -6,13 +6,13 @@ from random import randrange
 def _():
     @test.it('Fixed test')
     def _():
-        prisoners = [() for _ in range(100)]
+        prisoners = [[] for _ in range(100)]
         lightbulb = False
         assertion = False
         for i in range(29200):
             light_before = lightbulb
             lightbulb, assertion = solution.living_room(i % 100, lightbulb, prisoners[i % 100])
-            prisoners[i % 100] = (*prisoners[i % 100], light_before)
+            prisoners[i % 100].append(light_before)
             if assertion:
                 test.expect(i >= 100,
                             'One of the prisoners made a false assertion')
@@ -24,7 +24,7 @@ def _():
 def _():
     @test.it('Cheating tests')
     def _():
-        prisoners = [() for _ in range(100)]
+        prisoners = [[] for _ in range(100)]
         lightbulb = False
         assertion = False
         names_in_scope = ('living_room', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__')
@@ -37,14 +37,14 @@ def _():
             r = randrange(100)
             light_before = lightbulb
             lightbulb, assertion = solution.living_room(r, lightbulb, prisoners[r])
-            prisoners[r] = (*prisoners[r], light_before)
+            prisoners[r].append(light_before)
             if(not isinstance(lightbulb, bool) or not isinstance(assertion, bool)):
                 test.fail('living_room should return two booleans')
             if assertion:
                 break
 
         for _ in range(9):
-            prisoners = [() for _ in range(100)]
+            prisoners = [[] for _ in range(100)]
             lightbulb = False
             assertion = False
             skip = randrange(100)
@@ -54,7 +54,7 @@ def _():
                 light_and_assertion = solution.living_room(r, lightbulb, prisoners[r])
                 if r != skip:
                     lightbulb, assertion = light_and_assertion
-                    prisoners[r] = (*prisoners[r], light_before)
+                    prisoners[r].append(light_before)
                 if assertion:
                     test.fail('One of the prisoners made a false assertion')
                     break
@@ -64,21 +64,19 @@ def _():
     @test.it("Random tests")
     def _():
         for _ in range(9):
-            prisoners = [[() for _ in range(100)], [() for _ in range(100)]]
+            prisoners = [[[] for _ in range(100)], [[] for _ in range(100)]]
             lightbulbs = [False, False]
             assertions = [False, False]
-            entered = [[False for _ in range(100)], [False for _ in range(100)]]
             days = [0, 0]
             b = randrange(2)
             for _ in range(99999):
                 r = randrange(100)
                 light_before = lightbulbs[b]
                 lightbulbs[b], assertions[b] = solution.living_room(r, lightbulbs[b], prisoners[b][r])
-                prisoners[b][r] = (*prisoners[b][r], light_before)
-                entered[b][r] = True
+                prisoners[b][r].append(light_before)
                 days[b] += 1
                 if assertions[b]:
-                    test.expect(all(entered[b]),
+                    test.expect((all(p) for p in prisoners),
                                 'One of the prisoners made a false assertion')
                     b ^= 1
                 if not any(assertions):
